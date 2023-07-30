a4e4a3be5546fb9d58b5c3a798ff23eb1e8a6b23
kurisaki_kazuma <38486600+kult0922@users.noreply.github.com>
2023-07-30 09:26:39 +0900

    ルーティング挿入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,70 +1,19 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import useSWR from "swr";
-
-type Card = {
-  suit: string;
-  rank: number;
-};
-
-type Hand = {
-  cards: Card[];
-  name: string;
-};
-
-const getHandName = (url: string, card: Card[]): Promise<Hand> =>
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(card),
-  }).then((res) => res.json());
-
-const getFetcher = (url: string): Promise<Card[]> =>
-  fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => res.json());
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { HandDemo } from "./pages/hand-demo";
 
 function App() {
-  const { data, isLoading } = useSWR(
-    "http://localhost:8080/community-card",
-    getFetcher
+  return (
+    <BrowserRouter>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route path="/hand-demo">
+        <HandDemo />
+      </Route>
+    </BrowserRouter>
   );
-
-  const [hand, setHand] = useState<Hand | undefined>();
-  console.log(data);
-
-  useEffect(() => {
-    if (data) {
-      getHandName("http://localhost:8080/hand", data).then((res) => {
-        console.log(res);
-        setHand(res);
-      });
-    }
-  }, [data]);
-
-  if (isLoading) return <div>loading...</div>;
-  if (data)
-    return (
-      <div>
-        <div>
-          <h1>{hand?.name}</h1>
-        </div>
-        {data.map((e, idx) => {
-          return (
-            <img
-              key={"card-" + idx}
-              src={`/cards/${e.rank}_of_${e.suit}s.svg`}
-              alt="card"
-            />
-          );
-        })}
-      </div>
-    );
 
   return <div>failed to load</div>;
 }
---
+++ frontend/src/pages/hand-demo/index.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+type Card = {
+  suit: string;
+  rank: number;
+};
+
+type Hand = {
+  cards: Card[];
+  name: string;
+};
+
+const getHandName = (url: string, card: Card[]): Promise<Hand> =>
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(card),
+  }).then((res) => res.json());
+
+const getFetcher = (url: string): Promise<Card[]> =>
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+export const HandDemo = () => {
+  const { data, isLoading } = useSWR(
+    "http://localhost:8080/community-card",
+    getFetcher
+  );
+
+  const [hand, setHand] = useState<Hand | undefined>();
+  console.log(data);
+
+  useEffect(() => {
+    if (data) {
+      getHandName("http://localhost:8080/hand", data).then((res) => {
+        console.log(res);
+        setHand(res);
+      });
+    }
+  }, [data]);
+
+  if (isLoading) return <div>loading...</div>;
+  if (data)
+    return (
+      <div>
+        <div>
+          <h1>{hand?.name}</h1>
+        </div>
+        {data.map((e, idx) => {
+          return (
+            <img
+              key={"card-" + idx}
+              src={`/cards/${e.rank}_of_${e.suit}s.svg`}
+              alt="card"
+            />
+          );
+        })}
+      </div>
+    );
+
+  return <div>failed to load</div>;
+};
